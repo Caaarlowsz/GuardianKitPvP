d03a63d7b55f20add03f442e0273ba5d182c758f
MrUniverse44 <oficial.mruniverse@gmail.com>
2021-07-03 13:05:02 -0500

    File Improvements

--- src/main/java/dev/mruniverse/guardiankitpvp/GuardianKitPvP.java
+++ src/main/java/dev/mruniverse/guardiankitpvp/GuardianKitPvP.java
@@ -2,16 +2,13 @@ package dev.mruniverse.guardiankitpvp;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class GuardianKitPvP extends JavaPlugin {
+public class GuardianKitPvP extends JavaPlugin {
+
+
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
 
     }
 
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
-    }
 }
