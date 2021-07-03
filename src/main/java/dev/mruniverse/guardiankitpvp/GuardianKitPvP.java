fdc2a8d5ed8eb948aa2240a81beff22120bde50e
MrUniverse44 <oficial.mruniverse@gmail.com>
2021-07-03 11:00:37 -0500

    Initial commit

---
+++ src/main/java/dev/mruniverse/guardiankitpvp/GuardianKitPvP.java
@@ -0,0 +1,17 @@
+package dev.mruniverse.guardiankitpvp;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class GuardianKitPvP extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
